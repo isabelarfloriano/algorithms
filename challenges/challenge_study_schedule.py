8b327d3a0b17f3098a34099fd39bcbb004a1351c
Isabela Rozani Floriano <irf161295@gmail.com>
2023-01-27 20:48:32 -0300

    feat: create study_schedule to get the time with more students online

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui.REQ 1"""
-    raise NotImplementedError
+    students_counter = 0
+
+    if not target_time or not permanence_period:
+        return None
+
+    for entry, exit in permanence_period:
+        if ((type(entry) is not int) or (type(exit) is not int)):
+            return None
+        
+        if entry <= target_time <= exit:
+            students_counter += 1
+
+    return students_counter
