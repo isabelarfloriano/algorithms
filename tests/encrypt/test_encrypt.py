2925aba0b6f5a50220c99a54085ece51cbbc01fe
Isabela Rozani Floriano <irf161295@gmail.com>
2023-01-30 11:12:57 -0300

    test: implement the tests of a cryptographic function

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+
+    assert encrypt_message("buapau", 3) == "uap_aub" 
+    assert encrypt_message("buapau", 4) == "ua_paub"
+    assert encrypt_message("buapau", 9) == "uapaub"
+
+    with pytest.raises(TypeError):
+        encrypt_message(3, "buapau")
